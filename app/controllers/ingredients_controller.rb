b32ca3fad28a03b1d648e1d84d8dd2c2aab56e93
Hung Phan <hungpv@atmarkcafe.org>
2018-05-31 21:28:13 +0700

    complete ingredient

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,11 +1,17 @@
 class IngredientsController < ApplicationController
   before_action :set_ingredient, only: [:edit, :update, :show]
   def new
-  
+    @ingredient = Ingredient.new
   end
 
   def create
-
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.save
+      flash[:success] = "Ingredient was successfully created"
+      redirect_to ingredient_path(@ingredient)
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -13,7 +19,12 @@ class IngredientsController < ApplicationController
   end
 
   def update 
-
+    if @ingredient.update(ingredient_params)
+      flash[:success] = "Ingredient name was updated successfully"
+      redirect_to @ingredient
+    else
+      render 'edit'
+    end
   end
 
   def show
@@ -27,6 +38,9 @@ class IngredientsController < ApplicationController
 
   private
 
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
+  end
   def set_ingredient
     @ingredient = Ingredient.find(params[:id])
   end
