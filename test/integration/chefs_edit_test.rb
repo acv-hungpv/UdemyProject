c9373cd5029ecb11981543f794e85e29b62568e9
Hung Phan <hungpv@atmarkcafe.org>
2018-05-31 14:48:33 +0700

    complete authentication system with admin and tests

--- test/integration/chefs_edit_test.rb
+++ test/integration/chefs_edit_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 class ChefsEditTest < ActionDispatch::IntegrationTest
   def setup
     @chef = Chef.create!(chefname: "hung", email: "hungphan@gmail.com",password: "password", password_confirmation: "password")
+    @chef2 = Chef.create!(chefname: "hung2", email: "hungphan2@gmail.com",password: "password", password_confirmation: "password")
+    @admin_user = Chef.create!(chefname: "hung3", email: "hungphan3@gmail.com",password: "password", password_confirmation: "password", admin: true)
     
   end
 
@@ -16,15 +18,44 @@ class ChefsEditTest < ActionDispatch::IntegrationTest
     assert_select 'div.panel-body'
   end
 
-  # test "accept valid edit " do
-  #   sign_in_as(@chef, "password")
-  #   get edit_chef_path(@chef)
-  #   assert_template 'chefs/edit'
-  #   patch chef_path(@chef), params: { chef: {chefname: "hung", email: " hungphan@gmail.com"}}
-  #   assert_redirected_to @chef
-  #   assert_not flash.empty?
-  #   @chef.reload
-  #   assert_match "hung",@chef.chefname
-  #   assert_match "hungphan@gmail.com", @chef.email
-  # end
+  test "accept valid edit" do
+    sign_in_as(@chef, "password")
+    get edit_chef_path(@chef)
+    assert_template 'chefs/edit'
+    patch chef_path(@chef), params: { chef: {chefname: "hung", email: "hungphan@gmail.com"}}
+    assert_redirected_to @chef
+    assert_not flash.empty?
+    @chef.reload
+    assert_match "hung",@chef.chefname
+    assert_match "hungphan@gmail.com", @chef.email
+  end
+
+  test "accept edit attempt by admin user" do
+    sign_in_as(@admin_user, "password")
+    get edit_chef_path(@chef)
+    assert_template 'chefs/edit'
+    patch chef_path(@chef), params: { chef: {chefname: "hung4", email: "hungphan4@gmail.com"}}
+    assert_redirected_to @chef
+    assert_not flash.empty?
+    @chef.reload
+    assert_match "hung4",@chef.chefname
+    assert_match "hungphan4@gmail.com", @chef.email     
+
+  end
+
+  test "redirect edit attempt by another non-admin user" do 
+    sign_in_as(@chef2, "password")
+    #get edit_chef_path(@chef)
+    #assert_template 'chefs/edit'
+    updated_name = "joe"
+    updated_email = "joe@gmail.com"
+
+    patch chef_path(@chef), params: { chef: {chefname: updated_name, email: updated_email}}
+    assert_redirected_to chefs_path
+    assert_not flash.empty?
+    @chef.reload
+    assert_match "hung",@chef.chefname
+    assert_match "hungphan@gmail.com", @chef.email
+
+  end 
 end
--- test/integration/chefs_listing_test.rb
+++ test/integration/chefs_listing_test.rb
@@ -5,7 +5,9 @@ class ChefsListingTest < ActionDispatch::IntegrationTest
     @chef = Chef.create!(chefname:"mashrur", email: "mashrur@example.com",
                   password: "password", password_confirmation: "password")
     @chef2 = Chef.create!(chefname:"john", email: "john@example.com",
-                  password: "password", password_confirmation: "password")  
+                  password: "password", password_confirmation: "password") 
+    @admin_user = Chef.create!(chefname:"john1", email: "john1@example.com",
+                  password: "password", password_confirmation: "password", admin: true)   
   end
 
   test "should get chefs listing" do
@@ -15,13 +17,14 @@ class ChefsListingTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", chef_path(@chef2), text: @chef2.chefname.capitalize     
   end
 
-  # test "should delete chef" do 
-  #   get chefs_path
-  #   assert_template 'chefs/index'
-  #   assert_difference 'Chef.count', -1 do
-  #     delete chef_path(@chef2)
-  #   end
-  #   assert_redirected_to chefs_path
-  #   assert_not flash.empty?
-  # end 
+  test "should delete chef" do 
+    sign_in_as(@admin_user, "password")
+    get chefs_path
+    assert_template 'chefs/index'
+    assert_difference 'Chef.count', -1 do
+      delete chef_path(@chef2)
+    end
+    assert_redirected_to chefs_path
+    assert_not flash.empty?
+  end 
 end
